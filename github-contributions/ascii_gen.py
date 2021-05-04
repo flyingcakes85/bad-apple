32e688ff0f09fde3209051289435164424075d69
Snehit Sah <snehitsah@protonmail.com>
2021-05-04 10:18:32 +0530

    Works!

--- github-contributions/ascii_gen.py
+++ github-contributions/ascii_gen.py
@@ -25,11 +25,8 @@ def pixel_to_ascii(image):
 
 def main():
 
-    print ("var frames = [\n")
-
-    # for i in range(1, 5):
     for i in range(1, 6573):
-        print ( "[")
+        
         path = "../frames/frame-" + f"{i:04d}" + ".jpg"
         try:
             image = PIL.Image.open(path)
@@ -38,25 +35,11 @@ def main():
         # resize image
         image = resize(image)
 
-        # convert image to greyscale image
-        # convert greyscale image to ascii characters
         greyscale_image = to_greyscale(image)
         ascii_str = pixel_to_ascii(greyscale_image)
-        img_width = greyscale_image.width
-        ascii_str_len = len(ascii_str)
-        ascii_img = ""
-
-        # Split the string based on width  of the image
-        for i in range(0, ascii_str_len, img_width*2):
-            ascii_img += ascii_str[i:i+img_width*2] + \
-                "\n"
-
-        ascii_arr = ascii_img.replace(",\n", "],\n[")
-        ascii_img = "[" + ascii_arr[:-1]
-        ascii_arr = ascii_img[:-2]
-        print(ascii_arr)
-        print ( "],\n")
-
-    print("]")
+        
+        frameFile = open("./ascii_frames/" + f"{i:04d}" + ".txt", "w")
+        frameFile.write(ascii_str[:-1])
+        frameFile.close()
 
 main()
--- github-contributions/js_function.js
+++ github-contributions/js_function.js
@@ -1,32 +1,48 @@
-//function removeAllChildNodes(parent) {
-//    while (parent.firstChild) {
-//        parent.removeChild(parent.firstChild);
-//    }
-//}
 
 let i = 0
 let d = 0
 let br = 0
-let frameNumber = 0
+let frameNumber = 1
 
 function renderApple() {
 
     if (br == 4) {
+        br = 0
         return 0
     }
 
-    if (frameNumber > 599) {
+    if (frameNumber > 6572) {
+	console.log("conpleted")
+        frameNumber = 1
         return 0
+    } else if (frameNumber == 1){
+    	console.log("started")
     }
 
-    for (i = 0; i < 37; i++) {
-        for (d = 0; d < 50; d++) {
-            outerBox.children[d].children[i].setAttribute("data-level", frames[parseInt(frameNumber)][i][d])
-        }
-    }
-    frameNumber++
-    setTimeout((frameNumber) => {
-        renderApple()
-    }, 0.033)
+   // console.log(frameNumber)
+
+    let frames = []
+
+    fetch('http://127.0.0.1/ascii_frames/' + frameNumber.toString().padStart(4, "0") + '.txt')
+        .then(res => res.blob())
+        .then(blob => {
+            blob.text().then(data => {
+
+                frames = data.split(",")
+                frameNumber++
+                
+                for (i = 0; i < 37; i++) {
+                    for (d = 0; d < 50; d++) {
+                        let magicNumber = (i * 50) + d
+                        outerBox.children[d].children[i].setAttribute("data-level", frames[magicNumber])
+                    }
+                }
+                setTimeout(() => {
+                    renderApple()
+                }, 250)
+
+            })
+
+        });
 
 }
